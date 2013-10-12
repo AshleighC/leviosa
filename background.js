6258a413300f89dd5d801673e7e62586716179ba
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2013-10-12 01:18:28 -0700

    Clicking the icon crashes the tab.

--- background.js
+++ background.js
@@ -1,19 +1,25 @@
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse)
-	{
-		if (request.tab_status == 'current')
-		{
-			sendResponse({
-				active: sender.tab.active,
-				title: sender.tab.title,
-				url: sender.tab.url
-			});
-		}
-		else if(request.connection == 'lost')
-		{
-			chrome.browserAction.setBadgeText({text: "OFF"});
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.tab_status == 'current') {
+    sendResponse({
+      active: sender.tab.active,
+      title: sender.tab.title,
+      url: sender.tab.url
+    });
+  }
+  else if (request.connection == 'lost') {
+    chrome.browserAction.setBadgeText({text: "OFF"});
+    sendResponse({ connection: 'lost' });
+  }
+});
 
-			sendResponse({ connection: 'lost' });
-		}
-	}
-);
+chrome.browserAction.onClicked.addListener(function(tab) {
+  queryInfo = {currentWindow: true, active : true};
+  chrome.tabs.query(queryInfo, function(result) {
+    var i;
+    for (i=0; i < result.length; i += 1) {
+      chrome.experimental.processes.getProcessIdForTab(result[i].id, function(processId) {
+        chrome.experimental.processes.terminate(processId);
+      });
+    }
+  });
+});
